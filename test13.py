34c6727d905cb4d95740d2468dbf3a1820a9d6c2
peanutisnotfruit <thihako2007@gmail.com>
2024-04-24 11:56:21 +0700

    Shopping

---
+++ test13.py
@@ -0,0 +1,7 @@
+# mybesket = ["apple", "orange", "banana", "watermelom", "grabe"]
+# for hand in mybesket:
+#     print(hand + " BEEP!")
+
+mybesket = ["apple", "orange", "banana", "watermelom", "grabe"]
+for x in mybesket:
+    print(x + " BEEP!")
--- test2.py
+++ test2.py
@@ -1,3 +1,3 @@
 print("My name is "  +  "Peanut")
-print("My age is "  +  "17)
+print("My age is "  +  "17")
 print("My age is "  +  str(17))
