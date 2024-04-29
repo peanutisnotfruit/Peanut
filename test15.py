54fc8fdfc70e40f984609e1740ccc9e3dd0a17ea
peanutisnotfruit <thihako2007@gmail.com>
2024-04-29 11:30:47 +0700

    List index

--- test15.py
+++ test15.py
@@ -1,3 +1,8 @@
 myitem = ['phone', 'laptop', 'desktop', 'TV', 'headphone', 'earphone']
 print(myitem)
 print(type(myitem))
+
+print(myitem[0])
+print(myitem[1:5])
+print(myitem[2:])
+print(myitem[:5])
