900979c1236846bfe4933f2bad5f9f056adb499a
peanutisnotfruit <thihako2007@gmail.com>
2024-04-29 11:41:10 +0700

    29-4-2024

--- test15.py
+++ test15.py
@@ -1,8 +1,11 @@
 myitem = ['phone', 'laptop', 'desktop', 'TV', 'headphone', 'earphone']
-print(myitem)
-print(type(myitem))
+# print(myitem)
+# print(type(myitem))
 
-print(myitem[0])
-print(myitem[1:5])
-print(myitem[2:])
-print(myitem[:5])
+# print(myitem[0])
+# print(myitem[1:5])
+# print(myitem[2:])
+# print(myitem[:5])
+
+if 'TV' in myitem:
+    print("Yes it is")
