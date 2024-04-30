216966e3c1d9be3ceee4924308432ed32a5c9c1f
peanutisnotfruit <thihako2007@gmail.com>
2024-04-30 11:42:02 +0700

    30_4_2024

---
+++ test17.py
@@ -0,0 +1,18 @@
+user_info = {
+    'Name' : 'Peanut',
+    'Height' : '170',
+    'fav_pet' : {
+        'pet1' : 'snake',
+        'pet2' : 'dogs',
+        'pet3' : 'gecko'
+     },
+     'fav_artist' : {
+        'artist1' : 'Ashes Remain',
+        'artist2' : 'Revenscode',
+        'artist3' : '911'
+     }
+}
+
+print(user_info['fav_pet']['pet1'])
+print(user_info['fav_artist']['artist2'])
+print(user_info['Name'])
