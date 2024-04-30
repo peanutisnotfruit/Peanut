5e8730d3f73beb9d7a6f07b22b629f93890d4143
peanutisnotfruit <thihako2007@gmail.com>
2024-04-30 12:23:59 +0700

    30/4/24

---
+++ test20.py
@@ -0,0 +1,4 @@
+def user_info(name, mail, addr="Yangon"):
+    print("Your name is ", name)
+    print("Your mail is ", mail)
+    print("Your addr is ", addr)
---
+++ test21.py
@@ -0,0 +1,7 @@
+from test20 import user_info
+
+user_info("Peanut", "thihako2007@gmail.com", "Maesot")
+
+user_info(addr="Maesot", name="Peanut", mail="thihako2007@gmail.com")
+
+user_info("Fruit", "Fruit@gmail.com")
